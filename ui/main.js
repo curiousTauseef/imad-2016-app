00d39f7f46b16c99c540eee7b50eb6c047e28f0a
kalamsuthar <kalamsuthar@gmail.com>
2016-09-23 18:41:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,15 @@ var element= document.getElementById("main-text");
 element.innerHTML="Praveen";
 //move the image
 var img = document.getElementById("madi");
+var marginLeft=0;
+function moveright(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+    
+}
+
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    //img.style.marginLeft='100px';
+    
+    var interval = setInterval(moveright,100);
 };
