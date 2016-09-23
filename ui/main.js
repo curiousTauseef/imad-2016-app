70f6133b4421624c801633530c026757ed3bb4dc
kalamsuthar <kalamsuthar@gmail.com>
2016-09-23 18:33:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //changing the text
 var element= document.getElementById("main-text");
 element.innerHTML="Praveen";
+//move the image
+var img = document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
