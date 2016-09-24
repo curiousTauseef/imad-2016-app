612195cf2cb2cbab53ac24cc90bbb49ffd79ac05
kalamsuthar <kalamsuthar@gmail.com>
2016-09-24 15:27:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,15 @@ app.get('/counter',function(req,res){
    res.send(counter.toString());
 });
 
+var names =[];
+app.get('/submit-name',function(req,res){//Submit : /submit-name?nmae=xxx
+    
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
+
 
 app.get('/:articleName',function(req,res){
     //articleNamw==article-one
@@ -123,14 +132,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names =[];
-app.get('/submit-name/:name',function(req,res){
-    
-    var name = req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
 
 
 
