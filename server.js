62118e4f8f77bea49dd1654a606fb0113def19ac
kalamsuthar <kalamsuthar@gmail.com>
2016-09-24 15:23:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var nmaes =[];
+var names =[];
 app.get('/submit-nmae/:name',function(req,res){
     
     var name = req.params.name;
